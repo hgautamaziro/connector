89e8c66899eba6cacd738e50aed4268e4d3f2734
Himanshi Gautam <hgautam@AD.MSYSTECHNOLOGIES.COM>
2026-04-20 10:45:09 +0530

    new updates

--- include/util.hpp
+++ include/util.hpp
@@ -3,7 +3,4 @@
 #include <unordered_set>
 using namespace std;
 
-using namespace std;
-//string generateUniqueBlobName(const std::string& filename);
-std::string generateDuplicateName(const std::string& filename,
-                                  std::unordered_set<std::string>& existingNames);
+string generateDuplicateName(const string& filename, unordered_set<std::string>& existingNames);
--- src/util.cpp
+++ src/util.cpp
@@ -3,17 +3,7 @@
 #include<chrono>
 using namespace std;
 
-// string generateUniqueBlobName(const std::string& filename)
-// {
-//     auto now = chrono::system_clock::now();
-//     auto ts = chrono::duration_cast<chrono::seconds>(
-//               now.time_since_epoch()).count();
- 
-//     return to_string(ts) + "_" + filename;
-// }
-
-std::string generateDuplicateName(const std::string& filename,
-                                         std::unordered_set<std::string>& existingNames)
+std::string generateDuplicateName(const std::string& filename, std::unordered_set<std::string>& existingNames)
 {
     if (existingNames.find(filename) == existingNames.end()) {
         existingNames.insert(filename);
@@ -22,19 +12,16 @@ std::string generateDuplicateName(const std::string& filename,
  
     int count = 1;
     std::string newName;
- 
     size_t dotPos = filename.find_last_of('.');
     std::string name = filename.substr(0, dotPos);
     std::string ext = (dotPos != std::string::npos) ? filename.substr(dotPos) : "";
  
     while (true) {
         newName = name + "_" + std::to_string(count) + ext;
- 
         if (existingNames.find(newName) == existingNames.end()) {
             existingNames.insert(newName);
             return newName;
         }
- 
         count++;
     }
 }
