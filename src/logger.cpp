a73024bb72e5815211e59657eca5bff76a841dc5
Himanshi Gautam <hgautam@AD.MSYSTECHNOLOGIES.COM>
2026-04-20 10:37:37 +0530

    new updates

--- src/logger.cpp
+++ src/logger.cpp
@@ -6,18 +6,14 @@
  
 void log(LogLevel level, const std::string& msg)
 {
-    // 🔹 Get current time
     auto now = std::chrono::system_clock::now();
     auto time = std::chrono::system_clock::to_time_t(now);
  
     std::tm tm{};
 #ifdef _WIN32
     localtime_s(&tm, &time);
-#else
-    localtime_r(&time, &tm);
 #endif
- 
-    // 🔹 Convert level to string
+
     const char* levelStr = "";
     switch (level) {
         case LogLevel::DEBUG: levelStr = "DEBUG"; break;
@@ -26,7 +22,7 @@ void log(LogLevel level, const std::string& msg)
         case LogLevel::ERR: levelStr = "ERROR"; break;
     }
  
-    // 🔹 Get thread id
+    //Get thread id
     auto threadId = std::this_thread::get_id();
     std::cerr << "["
               << std::put_time(&tm, "%Y-%m-%d %H:%M:%S") << "]"
